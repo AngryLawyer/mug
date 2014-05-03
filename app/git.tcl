e40dc63f3b564f5169be8e66bca99fcb52ccbfda
Tony Aldridge <zaragopha@hotmail.com>
2014-05-03 12:11:16 +0100

    Passing happy tests

--- app/git.tcl
+++ app/git.tcl
@@ -2,6 +2,19 @@ namespace eval ::mug::git {
     namespace export is_git_url
 
     proc is_git_url {url} {
+        set VALID_PREFIXES {
+            "git:"
+            "git+http:"
+            "git+https:"
+            "git+rsync:"
+            "git+ftp:"
+            "git+ssh:"
+        }
+
+        set prefix "[lindex [split $url ":"] 0]:"
+        if {[lsearch $VALID_PREFIXES $prefix] > -1} {
+            return 1
+        }
         return 0
     }
 }
